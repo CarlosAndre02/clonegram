f1bad950fb18aa31d4e27ed021ad2e2e2a3541bf
Carlos André <cagd2002@gmail.com>
2022-10-03 17:44:51 -0300

    feat: Add server with koa and graphql helix configs

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello World');
+import app from './server';
+import { config } from './config/env';
+
+app.listen(config.PORT, async () => {
+  console.log(`Server is running on port ${config.PORT}`);
+});
---
+++ packages/server/src/server.ts
@@ -0,0 +1,45 @@
+import Koa from 'koa';
+import cors from '@koa/cors';
+import bodyparser from 'koa-bodyparser';
+import {
+  getGraphQLParameters,
+  processRequest,
+  renderGraphiQL,
+  sendResult,
+  shouldRenderGraphiQL
+} from 'graphql-helix';
+
+import { schema } from './schema/schema';
+
+const app = new Koa();
+
+app.use(bodyparser());
+app.use(cors());
+
+app.use(async (ctx) => {
+  const request = {
+    body: ctx.request.body,
+    headers: ctx.req.headers,
+    method: ctx.request.method,
+    query: ctx.request.query
+  };
+
+  if (shouldRenderGraphiQL(request)) {
+    ctx.body = renderGraphiQL({});
+  } else {
+    const { operationName, query, variables } = getGraphQLParameters(request);
+
+    const result = await processRequest({
+      operationName,
+      query,
+      variables,
+      request,
+      schema
+    });
+
+    ctx.respond = false;
+    sendResult(result, ctx.res);
+  }
+});
+
+export default app;
