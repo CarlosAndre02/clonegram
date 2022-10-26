bead5cd21a7f3f9670610aec265ed8649a12e041
Carlos André <cagd2002@gmail.com>
2022-10-26 12:12:01 -0300

    feat(server): Add node and nodes field to QueryType

--- packages/server/src/schema/QueryType.ts
+++ packages/server/src/schema/QueryType.ts
@@ -1,12 +1,12 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLObjectType } from 'graphql';
+
+import { nodeField, nodesField } from '../modules/graphql/typeRegister';
 
 export const QueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'The root of all queries',
-  fields: {
-    hello_world: {
-      type: GraphQLString,
-      resolve: () => 'Hello world'
-    }
-  }
+  fields: () => ({
+    node: nodeField,
+    nodes: nodesField
+  })
 });
