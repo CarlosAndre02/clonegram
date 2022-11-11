4388e62e8da3973253aca18bf3d1591bb6d77195
Carlos André <cagd2002@gmail.com>
2022-11-11 15:40:54 -0300

    feat(server): Add me query

---
+++ packages/server/src/modules/user/queries/MeQuery.ts
@@ -0,0 +1,12 @@
+import { GraphQLFieldConfig } from 'graphql';
+
+import { UserType } from '../UserType';
+import UserLoader from '../UserLoader';
+
+export const me: GraphQLFieldConfig<any, any, any> = {
+  type: UserType,
+  resolve: (_, _args, context) => {
+    if (!context.user) return null;
+    return UserLoader.load(context, context.user?._id);
+  }
+};
--- packages/server/src/schema/QueryType.ts
+++ packages/server/src/schema/QueryType.ts
@@ -1,12 +1,14 @@
 import { GraphQLObjectType } from 'graphql';
 
 import { nodeField, nodesField } from '../modules/graphql/typeRegister';
+import * as userQueries from '@/modules/user/queries';
 
 export const QueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'The root of all queries',
   fields: () => ({
     node: nodeField,
-    nodes: nodesField
+    nodes: nodesField,
+    ...userQueries
   })
 });
