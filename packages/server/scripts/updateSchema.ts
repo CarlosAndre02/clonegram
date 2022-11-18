e7ff96594cef0b31a798ce742ff56980615cdeef
Carlos André <cagd2002@gmail.com>
2022-11-18 18:42:15 -0300

    chore(server): module alias suport for update schema script

--- packages/server/scripts/updateSchema.ts
+++ packages/server/scripts/updateSchema.ts
@@ -1,6 +1,8 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { printSchema } from 'graphql/utilities';
+import moduleAlias from 'module-alias';
+moduleAlias.addAlias('@', path.resolve(__dirname, '..', 'src'));
 
 import { schema } from '../src/schema/schema';
 
