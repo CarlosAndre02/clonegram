973c509d4ce104fc14ac2d7ba2e98b91413ab560
Carlos André <cagd2002@gmail.com>
2022-11-28 12:51:05 -0300

    feat(server): Add user update mutation

---
+++ packages/server/src/modules/user/mutations/UserUpdateMutation.ts
@@ -0,0 +1,62 @@
+import { GraphQLID, GraphQLNonNull, GraphQLString } from 'graphql';
+import { fromGlobalId, mutationWithClientMutationId } from 'graphql-relay';
+
+import { UserModel } from '../UserModel';
+import { UserType } from '../UserType';
+import { GraphQLContext } from '@/modules/graphql/types';
+
+export const UserUpdateMutation = mutationWithClientMutationId({
+  name: 'UpdateUser',
+  inputFields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID)
+    },
+    fullname: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    biography: {
+      type: new GraphQLNonNull(GraphQLString)
+    }
+  },
+  mutateAndGetPayload: async (
+    { id, fullname, biography },
+    ctx: GraphQLContext
+  ) => {
+    // TODO: Uncomment when login is implemented
+    // if (!ctx.user) {
+    //   return {
+    //     error: 'Sorry! You must be logged in to continue'
+    //   };
+    // }
+
+    const { id: userId } = fromGlobalId(id);
+    // if (ctx.user._id !== userId) {
+    //   return {
+    //     error: "Sorry! You're unauthorized to continue"
+    //   };
+    // }
+
+    const user = await UserModel.findOneAndUpdate(
+      { _id: userId },
+      { $set: { fullname, biography } },
+      { new: true, runValidators: true }
+    );
+
+    if (!user) return { error: 'User does not exist' };
+
+    return {
+      user,
+      error: null
+    };
+  },
+  outputFields: {
+    user: {
+      type: UserType,
+      resolve: ({ user }) => user
+    },
+    error: {
+      type: GraphQLString,
+      resolve: ({ error }) => error
+    }
+  }
+});
--- packages/server/src/modules/user/mutations/index.ts
+++ packages/server/src/modules/user/mutations/index.ts
@@ -1,1 +1,2 @@
 export * from './UserCreateMutation';
+export * from './UserUpdateMutation';
