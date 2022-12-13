8bfc4e00bf98c4d8d20d3f8039663a0f786a49f4
Carlos André <cagd2002@gmail.com>
2022-12-13 11:11:50 -0300

    refact(web): Edit profile edit page import in app routes

--- packages/web/src/Routes.tsx
+++ packages/web/src/Routes.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 const SignupPage = lazy(() => import('@/modules/auth/pages/SignupPage'));
 const ProfilePage = lazy(() => import('@/modules/user/pages/ProfilePage'));
 const ProfileEditPage = lazy(
-  () => import('@/modules/user/pages/ProfileEditPage')
+  () => import('@/modules/user/pages/ProfileEditPage/ProfileEditPage')
 );
 
 export const AppRoutes = () => (
