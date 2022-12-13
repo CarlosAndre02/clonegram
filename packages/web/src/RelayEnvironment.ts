041f244978e95952722e3f38ea4234c6bb56cadc
Carlos André <cagd2002@gmail.com>
2022-12-13 11:00:19 -0300

    feat(web): Implement uploads in fetch relay

--- packages/web/src/RelayEnvironment.ts
+++ packages/web/src/RelayEnvironment.ts
@@ -8,20 +8,58 @@ import {
 
 const HTTP_ENDPOINT = import.meta.env.VITE_API_BASE_URL;
 
-const fetchRelay: FetchFunction = async (request, variables) => {
-  const resp = await fetch(HTTP_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      query: request.text,
-      variables
-    })
-  });
+const fetchRelay: FetchFunction = async (
+  query,
+  variables,
+  cacheConfig,
+  uploadables
+) => {
+  const headers: any = {};
+  let body: any = {};
 
-  return await resp.json();
+  if (uploadables) {
+    if (!window.FormData) {
+      throw new Error('Uploading files without `FormData` not supported.');
+    }
+
+    const formData = new FormData();
+
+    const queryText = query?.text?.replace(/\n/g, '');
+    const operations = JSON.stringify({
+      query: queryText,
+      variables
+    });
+    formData.append('operations', operations);
+
+    // For now, supports only 1 upload, named as 'file'
+    const map = {
+      0: [`variables.input.file`]
+    };
+    formData.append('map', JSON.stringify(map));
+    formData.append('0', uploadables['file']);
+
+    body = formData;
+  } else {
+    headers['Content-Type'] = 'application/json';
+    body = JSON.stringify({
+      query: query.text,
+      variables
+    });
+  }
+
+  // if (token) {
+  //   headers['Authorization'] = `Bearer <token>`;
+  // }
+
+  const request = {
+    method: 'POST',
+    headers,
+    body
+  };
+
+  const resp = await fetch(HTTP_ENDPOINT, request);
+
+  return resp.json();
 };
 
 function createRelayEnvironment() {
