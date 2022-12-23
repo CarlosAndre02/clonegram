3c2204f3e36130c14d153a6fbaffa3934d1ea416
Carlos André <cagd2002@gmail.com>
2022-12-23 20:50:56 -0300

    feat(server): Add login mutation

---
+++ packages/server/src/modules/auth/mutations/AuthLoginMutation.ts
@@ -0,0 +1,38 @@
+import { GraphQLNonNull, GraphQLString } from 'graphql';
+import { mutationWithClientMutationId } from 'graphql-relay';
+
+import { createAccessToken, authenticateUser } from '../AuthService';
+import { AuthType } from '../AuthType';
+import { getUserByEmail } from '@/modules/user/UserService';
+import { BadRequestError } from '@/shared/AppErrors';
+
+export const AuthLoginMutation = mutationWithClientMutationId({
+  name: 'LoginUser',
+  inputFields: {
+    email: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    password: {
+      type: new GraphQLNonNull(GraphQLString)
+    }
+  },
+  mutateAndGetPayload: async ({ email, password }) => {
+    const user = await getUserByEmail(email);
+    if (!user) throw new BadRequestError('User does not exist');
+
+    if (!user.isPasswordValid(password))
+      throw new BadRequestError('Invalid credentials');
+
+    const accessToken = createAccessToken(user._id);
+    const authenticatedUser = await authenticateUser(user._id);
+    authenticatedUser['accessToken'] = accessToken;
+
+    return { token: authenticatedUser };
+  },
+  outputFields: {
+    token: {
+      type: AuthType,
+      resolve: ({ token }) => token
+    }
+  }
+});
--- packages/server/src/schema/MutationType.ts
+++ packages/server/src/schema/MutationType.ts
@@ -1,11 +1,13 @@
 import { GraphQLObjectType } from 'graphql';
 
 import * as userMutations from '@/modules/user/mutations';
+import * as authMutations from '@/modules/auth/mutations';
 
 export const MutationType = new GraphQLObjectType({
   name: 'Mutation',
   description: 'The root of all mutations',
   fields: () => ({
-    ...userMutations
+    ...userMutations,
+    ...authMutations
   })
 });
