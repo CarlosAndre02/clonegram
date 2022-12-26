7d297d9e46557f5bf04da8ebb1879cfb12652a6a
Carlos André <cagd2002@gmail.com>
2022-12-26 20:58:33 -0300

    feat(server): Change meQuery to return user in context

--- packages/server/src/modules/user/queries/MeQuery.ts
+++ packages/server/src/modules/user/queries/MeQuery.ts
@@ -1,12 +1,8 @@
 import { GraphQLFieldConfig } from 'graphql';
 
 import { UserType } from '../UserType';
-import UserLoader from '../UserLoader';
 
 export const me: GraphQLFieldConfig<any, any, any> = {
   type: UserType,
-  resolve: (_, _args, context) => {
-    if (!context.user) return null;
-    return UserLoader.load(context, context.user?._id);
-  }
+  resolve: (_, _args, context) => context.user
 };
