6b54f55e7d58487d356b5fa3a2937c6d0394c921
Carlos André <cagd2002@gmail.com>
2023-09-08 17:53:18 -0300

    chore(web): Move graphql schema to web package

--- packages/web/relay.config.js
+++ packages/web/relay.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   src: './src',
   language: 'typescript',
-  schema: '../../schema.graphql',
+  schema: './schema.graphql',
   exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
   eagerEsModules: true
 };
